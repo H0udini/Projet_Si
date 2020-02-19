7892768b15c41dabb1e1dc470d59dc48c84476ec
H0udini <47359436+H0udini@users.noreply.github.com>
2020-02-19 11:45:02 +0100

    Update Mongo.js

--- API/Mongo.js
+++ API/Mongo.js
@@ -1,1 +1,23 @@
 
+const mongoose = require('mongoose');
+const express = require('express');
+const body = require('body-parser');
+const  = require('./');
+
+let app = express();
+let port = 8016;
+
+app.use(body());
+
+mongoose.connect('mongodb://localhost:27017/M1_TYBAG_mongo', {useNewUrlParser: true});
+
+app.get('/:', async (req, res) => {
+    const  = req.params.; // on récupère la valeure dans l'url
+    const  = await Gare.findOne({_ : });
+    res.json(gare)
+ 
+})
+
+app.listen(port, () => {
+    console.log("C'est moi le meilleur serveur");
+})
