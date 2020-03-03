80a41d879edc80417f0496b0c37a7bd6a89bffce
H0udini <47359436+H0udini@users.noreply.github.com>
2020-03-03 11:52:54 +0100

    Update Mongo.js

--- API/Mongo.js
+++ API/Mongo.js
@@ -10,11 +10,31 @@ app.use(body());
 
 mongoose.connect('mongodb://localhost:27017/M1_TYBAG_mongo', {useNewUrlParser: true});
 
-app.get('/id_formation:', async (req, res) => {
-    const id = req.params.id; // on récupère la valeure dans l'url
-    const  = await Gare.findOne({_ : });
-    res.json(gare)
+app.post('/Projet', async(req, res) => {
+	const date = req.body.date; // récupération des variables du body
  
+    if (!date ) { 
+        res.send('Il manque un argument')
+        return
+    }
+ 
+    const nouveau_projet = new Projet({ // création d'un objet représentant notre nouveau livre
+        date : Date
+    })
+     
+    await nouveau_projet.save() // sauvegarde asynchrone du nouveau livre
+    res.json(nouveau_projet)
+    return
+})
+
+app.get('/', async (req, res) => {
+    const logs = await db.logs.find({});
+    res.json(logs)
+})
+
+app.get('/:date', async (req, res) => {
+    const logs = db.logs.findOne({"date" : "12:02:15"});
+    res.json(logs)
 })
 
 app.listen(port, () => {
