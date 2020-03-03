2da7887659796570cd4017562bdb8ae5923b781f
H0udini <47359436+H0udini@users.noreply.github.com>
2020-03-03 11:21:52 +0100

    Update MariaDB.js

--- API/MariaDB.js
+++ API/MariaDB.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 app.get('/Formations', async (req, res) => {
     connection.connect();
-    connection.query('SELECT nom_formation, description_formation FROM t_formation, function (error, results, fields) {
+    connection.query('SELECT nom_formation, description_formation FROM t_formation', function (error, results, fields) {
         if (error) throw error;   
         res.json(results);   
     })
@@ -30,7 +30,7 @@ app.get('/Formations', async (req, res) => {
 app.get('/:id_module', async (req, res) => {
     const id_module = req.param.id_module;
     connection.connect();
-    connection.query('SELECT nom_module, description_module FROM t_module Where id_module = +id_module, function (error, results, fields) {
+    connection.query('SELECT nom_module, description_module FROM t_module Where id_module = '+id_module, function (error, results, fields) {
         if (error) throw error;   
         res.json(results);   
     })
