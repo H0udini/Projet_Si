7178cf66d40f5921e6ab1e23c4cd8f527acbf6bd
H0udini <47359436+H0udini@users.noreply.github.com>
2020-03-05 14:53:16 +0100

    Update Neo4j.js

--- API/Neo4j.js
+++ API/Neo4j.js
@@ -10,7 +10,10 @@ let port = 8016;
 let session = driver.session();
 session
     .run(
-       'MATCH (f:TYBAG_FORMATIONS {id_formation : 1}), (m1:TYBAG_MODULES(id_module:5}),(m1)-[:NECESSITE]->(m2:TYBAG_MODULES), (f)-[:NECESSITE]->(m2) RETURN m2 AS identifiant'
+        'MATCH (f:TYBAG_FORMATIONS {id_formation : 1}), (m1:TYBAG_MODULES(id_module:5}),(m1)-[:NECESSITE]->(m2:TYBAG_MODULES), (f)-[:NECESSITE]->(m2) RETURN m2 AS identifiant';
+    )
+    .run(
+        'MATCH (f:TYBAG_FORMATIONS  {id_formation : 1})-[:CONTIENT]->(m:TYBAG_MODULES) return m AS je_sais_pas;
     )
     .subscribe({
       onKeys: keys => {
@@ -18,6 +21,9 @@ session
       },
       onNext: record => {
         console.log(record.get('identifiant'))
+      },
+      onNext: record => {
+        console.log(record.get('je_sais_pas'))
       },
       onCompleted: () => {
         session.close()
